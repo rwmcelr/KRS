c63cde6280c4363dd3bbd94c26de4bc7dd39f593
rwmcelr <75845442+rwmcelr@users.noreply.github.com>
2021-03-12 16:15:30 -0500

    Add files via upload

--- Volcano.R
+++ Volcano.R
@@ -30,25 +30,26 @@ sigGenes <- function(name, plots=T, filBy="padj", filVal=0.05, con1, con2) {
                             multiVals="first")
     maPoints <- as.data.frame(resMA)
     maPoints <- merge(x = maPoints, y = tf, by = "symbol", all.x = TRUE, no.dups = TRUE)
-    maPoints$identity[maPoints$pvalue.x < 0.05 & is.na(maPoints$pvalue.y)] <- "Significant (p < 0.05)"
     maPoints$identity[maPoints$pvalue.x < 0.05 & maPoints$identity == "NM23H2 Transcription Factor"] <- "Significant Transcription Factor"
     maPoints$negLog2pvalue <- log2(maPoints$pvalue.x) * -1
     png(file=paste0(name,"_VolcanoPlot.png"),width=1500,height=1500,res=150)
     plot2 <- ggplot(maPoints, aes(log2FoldChange.x, negLog2pvalue, colour=identity, label=symbol)) + 
-      ggtitle(paste0(name,"\nMA Plot")) +
+      ggtitle(paste0(name,"\nVolcano Plot")) +
       geom_point(size=1) +
-      # geom_text(aes(label=ifelse(is.na(identity) == FALSE & identity!="Significant",as.character(symbol),'')),hjust=1,vjust=-1) +
-      geom_label_repel(aes(label=ifelse(is.na(identity) == FALSE & identity!="Significant (p < 0.05)",as.character(symbol),'')),
+      geom_label_repel(aes(label=ifelse(identity=="Significant Transcription Factor",as.character(symbol),'')),
                        box.padding = 0.5,
                        point.padding = 0.5,
                        max.overlaps = Inf,
                        segment.color = 'grey50') +
       scale_y_continuous(limits=c(0, 15), oob=squish) + 
-      scale_x_continuous(limits=c(-5, 5)) + 
-      #geom_hline(yintercept = 0.5, colour="blue", size=1, linetype="dotted") + 
-      #geom_hline(yintercept = -0.5, colour="blue", size=1, linetype="dotted") + 
+      scale_x_continuous(limits=c(-5, 5), breaks=seq(-5,5,1)) + 
+      geom_hline(yintercept = 1, colour="yellow", size=1, linetype="longdash") + 
+      geom_text(label="P Value 0.5 Cutoff", x=-4.5, y=1.2, color="black") +
+      geom_hline(yintercept = c(4.322, 15.1), colour="skyblue", size=1, linetype="longdash") + 
+      geom_text(label="P Value 0.05 Cutoff", x=-4.4, y=4.522, color="black") +
       labs(x="Log2FoldChange", y="-Log2pvalue") + 
-      scale_colour_manual(name="Key", values=c("NM23H2 Transcription Factor"="orange","Significant (p < 0.05)"="skyblue","Significant Transcription Factor"="darkorchid1"), na.value="grey80") + 
+      scale_colour_manual(name="Key", values=c("NM23H2 Transcription Factor"="orange",
+                                               "Significant Transcription Factor"="darkorchid1"), na.value="grey80") + 
       theme_bw() +
       theme(plot.title = element_text(hjust = 0.5))
     print(plot2)
